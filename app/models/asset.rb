516873bdfb18f5b9c74126a5e57a0ce59dc6d1b8
J Travis Lindsey <reachjtl@gmail.com>
2015-05-24 10:23:27 -0400

    cleanup asset.rb

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -8,16 +8,6 @@ class Asset < Account
   end
 
   def self.types
-    # jtl this structure doesn't set me up to easily create user friendly drop down titles
-    # for examples if I want to say "Other Asset" i can't have that space without changing
-    # this method
-    # stuck with error after playing with this
-    # ActiveRecord::SubclassNotFound in Accounts#index
-    # The single-table inheritance mechanism failed to locate the subclass: 'Other
-    # line 16 on index page flagged
-    # ['Checking', 'Saving']
-
-    #attempted solution
     {
       'A Checking' => Checking, 
       'A Saving' => Saving
