c23cdb094c543249c1e0509410441b989f8394ef
J Travis Lindsey <reachjtl@gmail.com>
2015-05-24 12:50:14 -0400

    remove require for last4 due to cash and similar accounts, update view

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,6 +1,6 @@
 class Asset < Account
   validates :name, presence: true, length: {maximum: 45}
-  validates :last4, presence: true, length: {is: 4}
+  validates :last4, length: {maximum: 4}
   validates :status, inclusion: {in: ['Open', 'Closed']}
 
   def self.statuses
