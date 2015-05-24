8e1638f1b91398f5891ee2ecf6daa31fe38308d6
J Travis Lindsey <reachjtl@gmail.com>
2015-05-24 12:37:42 -0400

    change migration file and rake schema to specify decimal limit 8 so numbers larger than 99,999,999.00 can be used

--- db/migrate/20150519205420_create_accounts.rb
+++ db/migrate/20150519205420_create_accounts.rb
@@ -5,7 +5,7 @@ class CreateAccounts < ActiveRecord::Migration
       t.string :name, limit: 45
       t.string :last4, limit: 4
       t.string :status, :default => 'Open'
-      t.integer :balance_cents
+      t.integer :balance_cents, :limit => 8
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20150519205420) do
     t.string  "name",          limit: 45
     t.string  "last4",         limit: 4
     t.string  "status",                   default: "Open"
-    t.integer "balance_cents"
+    t.integer "balance_cents", limit: 8
   end
 
 end
