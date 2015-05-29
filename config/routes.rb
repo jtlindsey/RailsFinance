a527521c4b543522843bed555bd334378e5d5c45
J Travis Lindsey <reachjtl@gmail.com>
2015-05-29 09:51:39 -0400

    removed category scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :categories
   root 'accounts#index'
   resources :accounts
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529133431) do
+ActiveRecord::Schema.define(version: 20150526213532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,12 +24,6 @@ ActiveRecord::Schema.define(version: 20150529133431) do
     t.integer "balance_cents", limit: 8
     t.integer "credit_limit"
     t.decimal "interest_rate"
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
