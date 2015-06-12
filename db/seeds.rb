35edf48460399d5ceb2227eeda57452a94c36458
J Travis Lindsey <reachjtl@gmail.com>
2015-06-12 13:38:54 -0400

    added accounts to create with rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+accounts = Account.create([
+  { type: 'Checking', name: 'BOA seed', last4: '0214', balance: 2765.81}, 
+  { type: 'Saving', name: 'BOA seed', last4: '5214', balance: 473.00},
+  { type: 'Cash', name: 'MyCash seed', balance: 684.87},
+  { type: 'Escrow', name: 'WellsFargo seed', last4: '4321', balance: 3473.00},
+  { type: 'OtherAsset', name: 'Loan to Kate seed', balance: 500.00},
+  { type: 'CreditCard', name: 'Citi-Master seed', last4: '6384', balance: 2500.00},
+  { type: 'StudentLoan', name: 'Nelnet seed', balance: 5588.00},
+  { type: 'PersonalLoan', name: 'PNC seed', last4: '8465', balance: 1500.74},
+  { type: 'OtherLiability', name: 'Loan From Jim seed', balance: 942.58}
+  ])
