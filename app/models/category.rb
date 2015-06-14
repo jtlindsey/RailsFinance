19e36a540410f6089c1d90b1e98f99717e2eae30
jtlindsey <reachjtl@gmail.com>
2015-06-14 12:55:49 -0400

    added validation to category creating, added form select for types

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,8 @@
 class Category < ActiveRecord::Base
+  validates :name, presence: true, length: {maximum: 30}
+  validates :category_type, inclusion: {in: ['Income', 'Expense']}
+
+  def self.category_types
+    ['Income', 'Expense']
+  end
 end
