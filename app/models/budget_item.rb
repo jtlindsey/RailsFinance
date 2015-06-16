2050ef154b8051a2c600db2585e56ebccdc4ad20
jtlindsey <reachjtl@gmail.com>
2015-06-16 09:24:23 -0400

    added category list and periods with validation to budget

--- app/models/budget_item.rb
+++ app/models/budget_item.rb
@@ -1,3 +1,11 @@
 class BudgetItem < ActiveRecord::Base
-    monetize :amount_cents
+  validates :category, inclusion: {in: Category.order(:name).map {|category| category.name}, message: 'has not been selected' }
+  validates :period, inclusion: {in: %w(Weekly Monthly Quarterly Bi-Annually Yearly), message: 'has not been selected' }
+    
+  monetize :amount_cents
+
+  def self.period_list
+    #list of periods for form
+    %w(Weekly Monthly Quarterly Bi-Annually Yearly)
+  end
 end
