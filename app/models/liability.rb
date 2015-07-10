333271eb1ee0ebdb4e9b5574fee8fbefd2f7efe0
jtlindsey <reachjtl@gmail.com>
2015-07-10 17:29:16 -0400

    all account page updated liabilities

--- app/models/liability.rb
+++ app/models/liability.rb
@@ -3,17 +3,30 @@ class Liability < Account
   validates :last4, length: {maximum: 4}
   validates :status, inclusion: {in: ['Open', 'Closed']}
 
-  def self.statuses
-    ['Open', 'Closed']
+  # Class methods grouped here
+  class << self
+    def statuses
+      ['Open', 'Closed']
+    end
+
+    def types
+      {
+        'Personal Loan' => PersonalLoan, 
+        'Credit Card' => CreditCard,
+        'Student Loan' => StudentLoan,
+        'Other Liability' => OtherLiability,
+        'Mortgage (fixed)' => Mortgage
+      }
+    end
+
+    def account_type_groups
+      types.values.sort_by(&:name)
+    end
+
+    def total
+      Money.new(all.sum(:balance_cents))
+    end
   end
 
-  def self.types
-    {
-      'Personal Loan' => PersonalLoan, 
-      'Credit Card' => CreditCard,
-      'Student Loan' => StudentLoan,
-      'Other Liability' => OtherLiability,
-      'Mortgage (fixed)' => Mortgage
-    }
-  end
+  # Instance methods follow here
 end
