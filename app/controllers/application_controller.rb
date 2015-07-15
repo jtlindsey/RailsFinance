549197c1d6efffc9bf6b999b303977e31d6842f7
jtlindsey <reachjtl@gmail.com>
2015-07-15 00:44:16 -0400

    removed logout redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,9 @@ class ApplicationController < ActionController::Base
     [year, month, date].join("/")
   end
 
-  private
-  # Overwriting the sign_out redirect path method
-  def after_sign_out_path_for(resource_or_scope)
-    root_path
-  end
+  # private
+  # # Overwriting the sign_out redirect path method
+  # def after_sign_out_path_for(resource_or_scope)
+  #   root_path
+  # end
 end
