bff41865b2bc5a365d2007d4678ec8d9a60ed9c5
jtlindsey <reachjtl@gmail.com>
2015-07-19 23:02:59 -0400

    added helper for transactions total

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -2,4 +2,8 @@ module TransactionsHelper
   def current_user_account_transaction_path(extra_params)
     @transaction.id ? account_transaction_path(@account, @transaction, extra_params) : account_transactions_path(extra_params)
   end
+
+  def account_total(account_id)
+    Transaction.where(account_id: account_id).map {|transaction| transaction.applied_amount}.sum
+  end
 end
