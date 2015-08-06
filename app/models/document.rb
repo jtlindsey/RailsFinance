0ff2e9cef0e0ac37f0dccc9f9fe0b3634fd1f512
jtlindsey <reachjtl@gmail.com>
2015-08-06 17:01:39 -0400

    added validation file exist for carrierwave uploads

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,7 @@
 class Document < ActiveRecord::Base
   mount_uploader :efile, EfileUploader
+  validates_presence_of :efile
+
   # "You tried to define an association named transaction on the model Category, 
   # but this will conflict with a method transaction already defined by Active Record. 
   # Please choose a different association name."
