15214552b93c782e736597fe99adc29dd75c1270
jtlindsey <reachjtl@gmail.com>
2015-08-06 23:34:36 -0400

    updated uploader.rb and carrierwave.rb for auto dev and production storage type change

--- app/uploaders/efile_uploader.rb
+++ app/uploaders/efile_uploader.rb
@@ -9,7 +9,8 @@ class EfileUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
+  # storage :fog
+  # this is now being set in config/initializers/carrierwave.rb
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
