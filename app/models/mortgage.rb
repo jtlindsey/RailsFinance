ce1d482b25642591698239885bf30b81237607dd
jtlindsey <reachjtl@gmail.com>
2015-08-12 09:36:22 -0400

    updated new and edit account forms

--- app/models/mortgage.rb
+++ app/models/mortgage.rb
@@ -1,5 +1,5 @@
 class Mortgage < Liability
   def fields
-    super + %w(last4 mortgage_term loan_amount minimum_payment minimum_escrow_payment)
+    super + %w(last4 mortgage_term loan_amount interest_rate minimum_payment minimum_escrow_payment)
   end
 end
