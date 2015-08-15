837f04c764bff08d436c4d67be3c0f5f54d1de52
jtlindsey <reachjtl@gmail.com>
2015-08-15 12:44:25 -0400

    added validation to category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ActiveRecord::Base
   validates :name, presence: true, length: {maximum: 30}
+  validates_format_of :name, with: /\A[\sa-z \-'\ :]+\Z/i, message: 'can only contain letters, spaces and the following special characters - and :'
   validates :category_type, inclusion: {in: ['Income', 'Expense']}
 
   def self.category_types
