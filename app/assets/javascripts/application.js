c4194b5b2a6eb51c4f5f70f60e8ad861ce034a1f
jtlindsey <reachjtl@gmail.com>
2015-08-22 11:31:45 -0400

    adding gems and assets back after successful heroku deploy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require dataTables/jquery.dataTables
-//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require chartkick
 //= require turbolinks
 //= require bootstrap-sprockets
