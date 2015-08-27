c25c92c13a6c627582858ada033127ee7aed082d
jtlindsey <reachjtl@gmail.com>
2015-08-26 23:18:51 -0400

    add starter categories to new user account

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,32 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, length: {maximum: 20}
   validates_format_of :name, with: /\A[\sa-z0-9]+\Z/i, message: 'can only contain numbers and letters'
+
+  after_create :create_transaction_categories_for_user
+
+
+  def create_transaction_categories_for_user
+    # create starter categories for new users
+    user_id = self.id
+    Category.create!([
+      { user_id: user_id, category_type: 'Expense', name: 'Food-Grocery'},
+      { user_id: user_id, category_type: 'Expense', name: 'Food-Eatout'}, 
+      { user_id: user_id, category_type: 'Expense', name: 'Automotive-Maintenance'},
+      { user_id: user_id, category_type: 'Expense', name: 'Automotive-Gas'},
+      { user_id: user_id, category_type: 'Expense', name: 'Automotive-Insurance'},
+      { user_id: user_id, category_type: 'Expense', name: 'Bank-Fees'},
+      { user_id: user_id, category_type: 'Expense', name: 'Utilities-Electric'},
+      { user_id: user_id, category_type: 'Expense', name: 'Utilities-Water'},
+      { user_id: user_id, category_type: 'Expense', name: 'Utilities-Gas'},
+      { user_id: user_id, category_type: 'Expense', name: 'Yard-Maintenance'},
+      { user_id: user_id, category_type: 'Expense', name: 'Phone'},
+      { user_id: user_id, category_type: 'Expense', name: 'Education'},
+      { user_id: user_id, category_type: 'Expense', name: 'Other'},
+      { user_id: user_id, category_type: 'Income', name: 'PrimaryJob'},
+      { user_id: user_id, category_type: 'Income', name: 'Bank-Interest'},
+      { user_id: user_id, category_type: 'Income', name: 'Other-Income'},
+      { user_id: user_id, category_type: 'Income', name: 'Opening-Deposit'}    
+    ])
+  end
+
 end
