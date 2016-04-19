906625ca77e82214d69d285cfc594f168eae6c9e
jtlindsey <reachjtl@gmail.com>
2016-04-19 10:58:18 -0400

    disabled signups

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,6 @@
-# class RegistrationsController < Devise::RegistrationsController
-#   def new
-#     flash[:danger] = "*********New registrations are not allowed*********"
-#     redirect_to root_path
-#   end
-# end
+class RegistrationsController < Devise::RegistrationsController
+  def new
+    flash[:danger] = "*********New registrations are not allowed*********"
+    redirect_to root_path
+  end
+end
--- config/initializers/devise_permitted_parameters.rb
+++ config/initializers/devise_permitted_parameters.rb
@@ -8,7 +8,7 @@ module DevisePermittedParameters
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
+    #devise_parameter_sanitizer.for(:sign_up) << :name
     devise_parameter_sanitizer.for(:account_update) << :name
   end
 
